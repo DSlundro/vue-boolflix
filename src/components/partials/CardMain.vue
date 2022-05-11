a2f733e3a15392ed72c34fa5d40b992bf151d544
DSlundro <dragan.savic95@outlook.it>
2022-05-11 18:41:22 +0200

    update card

--- src/components/partials/CardMain.vue
+++ src/components/partials/CardMain.vue
@@ -1,46 +1,57 @@
 <template>
     <div class="col pt-5" >
-        <div class="flip-card">
-            <div class="my-card flip-card-inner">
+        <!-- CARD CONTAINER -->
+        <div class="my-card flip-card">
+            <!-- FLIPPING CARD SECTION -->
+            <div class="flip-card-inner">
+                <!-- FRONT CARD -->
                 <div class="flip-card-front">
                     <img v-if="movie.poster_path" :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`" >
                     <h2 v-else>Missing image</h2>
                 </div>
-                <div class="back flip-card-back p-4 bg-dark">
 
+                <!-- BACK CARD -->
+                <div class="flip-card-back p-4 bg-dark">
+                    
+                    <!-- TITLE -->
                     <div class="title fs-5">
-                    <h6 v-if="movie.title"><b>Titolo:</b> {{movie.title}}</h6>
-                    <h6 v-else><b>Titolo:</b> {{movie.name}}</h6>
-                </div>
-                
-                <div class="original_title">
-                    <h6 v-if="movie.original_title && movie.original_title != movie.title"><b>Titolo originale:</b> {{movie.original_title}}</h6>
-                    <h6 v-if="movie.original_name && movie.original_name != movie.name"><b>Titolo originale:</b> {{movie.original_name}}</h6>
-                </div>
-
-                <div class="lang d-flex">
-                    <div class="pe-2"><b>Lingua originale:</b>
-                    </div><lang-flag :iso="movie.original_language"/>
-                </div>
-
-                <div class="rating d-flex align-items-center">
-                    <div class="pe-2 pt-1"><b>Voto:</b> {{movie.vote_average}}</div>
-                    <rate :length="5" :value="starRating(movie.vote_average)" :readonly="true"></rate>
-                </div>
-
-                <div class="cast py-1">
-                    <div class="d-flex flex-wrap">
-                    <h6 class="m-0 p-0 pe-1"><b>Cast: </b></h6>
-                    <div class="person movies_cast pe-1" v-for="actor in movie.cast" :key="actor.id">
-                    {{actor.name}},
+                        <h6 v-if="movie.title"><b>Titolo:</b> {{movie.title}}</h6>
+                        <h6 v-else><b>Titolo:</b> {{movie.name}}</h6>
                     </div>
+                    
+                    <!-- ORIGINAL TITLE -->
+                    <div class="original_title">
+                        <h6 v-if="movie.original_title && movie.original_title != movie.title"><b>Titolo originale:</b> {{movie.original_title}}</h6>
+                        <h6 v-if="movie.original_name && movie.original_name != movie.name"><b>Titolo originale:</b> {{movie.original_name}}</h6>
                     </div>
-                </div>
 
-                <div class="overview pt-1">
-                    <p v-if="movie.overview"><b class="fs-6 pe-1">Overview:</b>{{movie.overview}}</p>
-                    <p v-else>Descrizione mancante!</p>
-                </div>
+                    <!-- ORIGINAL LANGUAGE -->
+                    <div class="lang d-flex">
+                        <div class="pe-2"><b>Lingua originale:</b>
+                        </div><lang-flag :iso="movie.original_language"/>
+                    </div>
+
+                    <!-- VOTE AVERAGE -->
+                    <div class="rating d-flex align-items-center">
+                        <div class="pe-2 pt-1"><b>Voto:</b> {{movie.vote_average}}</div>
+                        <rate :length="5" :value="starRating(movie.vote_average)" :readonly="true"></rate>
+                    </div>
+                    
+                    <!-- ACTORS-->
+                    <div class="cast py-1">
+                        <div class="d-flex flex-wrap">
+                        <h6 class="m-0 p-0 pe-1"><b>Cast: </b></h6>
+                        <div class="person movies_cast pe-1" v-for="actor in movie.cast" :key="actor.id">
+                        {{actor.name}},
+                        </div>
+                        </div>
+                    </div>
+
+                    <!-- OVERVIEW -->
+                    <div class="overview pt-1">
+                        <p v-if="movie.overview"><b class="fs-6 pe-1">Overview:</b>{{movie.overview}}</p>
+                        <p v-else>Descrizione mancante!</p>
+                    </div>
 
                 </div>
             </div>
@@ -65,35 +76,37 @@ export default {
 <style lang="scss">
 .my-card{
     cursor: pointer;
-    .back{
-    lang-flag{width: 32px; height: 24px; border: 1px solid gray;}
+    .flip-card-front{
+        img{
+        width: 100%;
+        height: 350px;
+        }
     }
-    img{
-    width: 100%;
-    height: 350px;
-    }
-    .Rate{
-    button{
-        margin: 0 !important;padding: 0 !important; cursor: initial !important;
-    }
-    svg.icon{margin: 0 2px !important;}
-    }
-    h2{
-    background-color: $dark;
-    color: $danger;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    }
-    .lang {font-size: 15px;}
-    .overview p{font-size: 12px; text-align: justify;}
-    .cast .person{
-    font-size: 12px;
+    .flip-card-back{
+        lang-flag{width: 32px; height: 24px; border: 1px solid gray;}
+        .Rate{
+        button{
+            margin: 0 !important;padding: 0 !important; cursor: initial !important;
+        }
+        svg.icon{margin: 0 2px !important;}
+        }
+        h2{
+        background-color: $dark;
+        color: $danger;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        }
+        .lang {font-size: 15px;}
+        .overview p{font-size: 12px; text-align: justify;}
+        .cast .person{
+        font-size: 12px;
+        }
     }
 }
 
-// FLIPPING CARD
+// FLIPPING CARD ANIMATION
 .flip-card {
 background-color: transparent;
 height: 350px;
